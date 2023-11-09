c54b6c5e13aae5e84474c45c14c5396776684334
Gene Bogdanovich <genebogdanovich@gmail.com>
2023-11-09 14:07:55 +0300

    Initial Commit

---
+++ TextSquares/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextSquares
+//
+//  Created by Gene Bogdanovich on 9.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextSquares/TextSquaresApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextSquaresApp.swift
+//  TextSquares
+//
+//  Created by Gene Bogdanovich on 9.11.23.
+//
+
+import SwiftUI
+
+@main
+struct TextSquaresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
