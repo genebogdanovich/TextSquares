838576085fed185b4290fcedb6df572131d2851a
Gene Bogdanovich <genebogdanovich@gmail.com>
2023-11-09 14:28:45 +0300

    Create sample project.

--- TextSquares/ContentView.swift
+++ TextSquares/ContentView.swift
@@ -7,17 +7,35 @@
 
 import SwiftUI
 
+// MARK: - ContentView
+
 struct ContentView: View {
+    
+    let sets = [1,3,6,8,12,16,23,43,56,76,100,101,125]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(sets, id: \.self) { set in
+                        Text("\(set)")
+                            .padding(8)
+                            .background(Color.blue)
+                    }
+                }
+            }
+            
+            Color.green
+                .overlay {
+                    Text("56")
+                        .font(.largeTitle.weight(.bold))
+                }
         }
-        .padding()
     }
 }
+
+// MARK: - Preview
 
 #Preview {
     ContentView()
